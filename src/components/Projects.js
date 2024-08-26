3ace15193b9dc225acced79273fa5400eee668c3
Daniel <official130496@gmail.com>
2024-08-26 10:41:30 +0800

    Updated project list and fixed image loading issues

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,23 +1,7 @@
 import React from "react";
 import "./css/Projects.css";
+import projectList from "./projectData";
 const Projects = () => {
-  const projectList = [
-    {
-      title: "Project One",
-      description:
-        "A brief description of Project One. This project demonstrates my skills in HTML, CSS, and JavaScript.",
-      imageUrl: "path/to/project-one-image.jpg",
-      link: "https://github.com/yourusername/project-one",
-    },
-    {
-      title: "Project Two",
-      description:
-        "A brief description of Project Two. This project showcases my ability to work with React and build dynamic web applications.",
-      imageUrl: "path/to/project-two-image.jpg",
-      link: "https://github.com/yourusername/project-two",
-    },
-  ];
-
   return (
     <div id='projects' className="projects">
       <h2>My Project</h2>
---
+++ src/components/projectData.js
@@ -0,0 +1,20 @@
+// projectData.js
+const projectList = [
+    {
+      title: "Sign Up Form",
+      description:
+        "This project is a user-friendly sign-up form built with React. Its includes form validation and responsive design to ensure a seamless user experience across different devices.",
+      imageUrl: process.env.PUBLIC_URL + "/signup.jpg",
+      link: "https://github.com/Daniel130496/signup-form",
+    },
+    {
+      title: "Project Two",
+      description:
+        "A brief description of Project Two. This project showcases my ability to work with React and build dynamic web applications.",
+      imageUrl: "path/to/project-two-image.jpg",
+      link: "https://github.com/yourusername/project-two",
+    },
+  ];
+  
+  export default projectList;
+  
