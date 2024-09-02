d24d3ce733a295892c5805ed7602b6ab1c85de79
Daniel <official130496@gmail.com>
2024-09-02 21:15:09 +0800

    update project and components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { FaEnvelope, FaPhone } from 'react-icons/fa';
 import "./css/Contact.css";
+import emailIcon from "../assets/images/email.png";
+import phoneIcon from "../assets/images/phone.png";
+import githubIcon from "../assets/images/github.png";
 
 const Contact = () => {
-    return(
+    return (
         <div id="contact" className="contact">
             <h2>Contact Me</h2>
             <div className="contact-icons">
                 <a href="mailto:official130496@gmail.com" className="icon">
-                    <FaEnvelope size={30} />
+                    <img src={emailIcon} alt="email icon" />
                 </a>
                 <a href="tel:+60149960832" className="icon">
-                    <FaPhone size={30} />
+                    <img src={phoneIcon} alt="smartphone icon" />
+                </a>
+                <a href="https://github.com/Daniel130496" className="icon">
+                    <img src={githubIcon} alt="github" />
                 </a>
             </div>
         </div>
-    )
-}
+    );
+};
+
 export default Contact;
--- src/components/projectData.js
+++ src/components/projectData.js
@@ -5,14 +5,20 @@ const projectList = [
       description:
         "This project is a user-friendly sign-up form built with React. Its includes form validation and responsive design to ensure a seamless user experience across different devices.",
       imageUrl: process.env.PUBLIC_URL + "/signup.jpg",
-      link: "https://github.com/Daniel130496/signup-form",
+      link: "https://daniel130496.github.io/signup-form/",
     },
     {
       title: "Price Card",
       description:
         "This project is pricing plan card built with React. It includes monthly and yearly plan with toggle button to switch between the two plans.",
       imageUrl: process.env.PUBLIC_URL + "/pricecard.jpg",
-      link: "https://github.com/Daniel130496/price-card",
+      link: "https://daniel130496.github.io/price-card/",
+    },
+    {
+      title: "Weather App",
+      description: "This project is a simple Weather App built with React that allow users to search for weathher information by city",
+      imageUrl: process.env.PUBLIC_URL + "/weather.jpg",
+      link: "https://daniel130496.github.io/weather-app/",
     },
   ];
   
